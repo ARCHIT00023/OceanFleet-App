f532dfb87d8de43390e622b70d243e5e51db95df
Archit Singh <architsingh00023@gmail.com>
2026-02-03 15:10:06 +0530

    [Archit] :feat(Dev) : UC-1 Define Vessel model using encapsulation and constructors

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,59 @@
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("UC-1: Project setup");
+/**
+ * UC1: Create Vessel Model
+ * Demonstrates Class, Encapsulation, Constructors,
+ * Getter & Setter methods following JavaBeans convention
+ */
+public class Vessel {
+
+    // Instance variables (attributes)
+    private String vesselId;
+    private String vesselName;
+    private double averageSpeed;
+    private String vesselType;
+
+    // No-argument constructor
+    public Vessel() {
+    }
+
+    // Parameterized constructor
+    public Vessel(String vesselId, String vesselName, double averageSpeed, String vesselType) {
+        this.vesselId = vesselId;
+        this.vesselName = vesselName;
+        this.averageSpeed = averageSpeed;
+        this.vesselType = vesselType;
+    }
+
+    // Getter and Setter methods (JavaBeans convention)
+
+    public String getVesselId() {
+        return vesselId;
+    }
+
+    public void setVesselId(String vesselId) {
+        this.vesselId = vesselId;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getVesselType() {
+        return vesselType;
+    }
+
+    public void setVesselType(String vesselType) {
+        this.vesselType = vesselType;
     }
 }
